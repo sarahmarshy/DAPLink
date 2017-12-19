631e8bdaf4841ee267359d6c61a9938b7b95dc99
Sarah Marsh <Sarah.Marsh@arm.com>
2017-12-19 16:31:02 -0600

    Update flash_blob to include sectors

--- source/target/nxp/lpc4088/flash_blob.c
+++ source/target/nxp/lpc4088/flash_blob.c
@@ -1,78 +1,74 @@
-/**
- * @file    flash_blob.c
- * @brief   Flash algorithm for the lpc4088
+/* Flash OS Routines (Automagically Generated)
+ * Copyright (c) 2009-2015 ARM Limited
  *
- * DAPLink Interface Firmware
- * Copyright (c) 2009-2016, ARM Limited, All Rights Reserved
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-#include "flash_blob.h"
-
-
-static const uint32_t lpc4088_flash_prog_blob[] = {
+static const uint32_t _flash_prog_blob[] = {
     0xE00ABE00, 0x062D780D, 0x24084068, 0xD3000040, 0x1E644058, 0x1C49D1FA, 0x2A001E52, 0x4770D1F2,
-    0x28100b00, 0x210ebf24, 0x00d0eb01, 0xe92d4770, 0xf8df4df0, 0x46068320, 0x460c44c8, 0x0000f8d8, 
-    0x1c402500, 0x0f01f010, 0x0000f8c8, 0x461749c2, 0x2080f44f, 0x63c8bf14, 0x05306388, 0xa2fcf8df, 
-    0xf04f0d00, 0x44ca0b00, 0xf8cad111, 0xf44fb010, 0xf8ca5080, 0xe9ca6000, 0xf8ca0b01, 0xf8d8b00c, 
-    0x4651000c, 0xf1a16882, 0x47900080, 0x2018b9c0, 0xb008f8ca, 0xb003e9ca, 0xf5b4b1cc, 0xbf8c7f80, 
-    0x7b80f44f, 0x197046a3, 0x0b00e9ca, 0x000cf8d8, 0x19794aaa, 0x6843444a, 0x0080f1a2, 0xb1104798, 
-    0xe8bd2001, 0x445d8df0, 0x040bebb4, 0x2000d1e5, 0x8df0e8bd, 0x41f0e92d, 0x8278f8df, 0x60e0f642, 
-    0x4d9f44c8, 0x0008f8c8, 0xf8052000, 0x20aa0f80, 0x20557328, 0x20017328, 0x0c40f805, 0x21122200, 
-    0xf91af000, 0x210d2200, 0xf0004610, 0x2200f915, 0x2001210d, 0xf910f000, 0x21132200, 0xf0002002, 
-    0x4c8df90b, 0xf4406820, 0x60205000, 0xf4406a20, 0x62202084, 0xf4406c20, 0x64202000, 0x2780f44f, 
-    0x117e63e7, 0x6c6861a6, 0x3080f440, 0x20026468, 0x0134f8c5, 0x21072205, 0xf8eef000, 0x21162205, 
-    0xf0002000, 0x2205f8e9, 0x2000210f, 0xf8e4f000, 0x21102205, 0xf0002000, 0x2205f8df, 0x20002111, 
-    0xf8daf000, 0x21122205, 0xf0002000, 0x4875f8d5, 0x727af44f, 0x69406800, 0x000cf8c8, 0x1008f8d8, 
-    0xf1f2fbb1, 0x2134f8d5, 0xc000f8d0, 0x021ff002, 0xf3f2fbb1, 0x486d496c, 0x2103fba1, 0x22c00889, 
-    0x47e04448, 0xbf042800, 0xe8bd61e6, 0x63a781f0, 0x200161e6, 0x81f0e8bd, 0x47702000, 0x41f0e92d, 
-    0x20324c63, 0x2700444c, 0x60a5251d, 0x0700e9c4, 0xf1044e60, 0x46200114, 0x696047b0, 0x2034b980, 
-    0xe9c460a5, 0x48530700, 0x0114f104, 0x68804448, 0x462060e0, 0x696047b0, 0xbf082800, 0x81f0e8bd, 
-    0xe8bd2001, 0xf1b081f0, 0xbf325f20, 0x2f00f5b0, 0x47702000, 0x0b04b570, 0xbf242c10, 0xeb00200e, 
-    0x203204d4, 0x4e4b4d4a, 0xf105444d, 0xe9c50114, 0x46280400, 0x47b060ac, 0xb9786968, 0xe9c52034, 
-    0x483c0400, 0x444860ac, 0x0114f105, 0x60e86880, 0x47b04628, 0x28006968, 0xbd70bf08, 0xbd702001, 
-    0x41f0e92d, 0x46054f33, 0x4614444f, 0x4a326878, 0xf0101c40, 0x60780f01, 0x5000f44f, 0x61d0bf14, 
-    0xf1b56190, 0xd3055f20, 0xf1a54622, 0xe8bd5020, 0xe6bc41f0, 0x2f00f5b5, 0x4622d305, 0x2000f5a5, 
-    0x41f0e8bd, 0xb975e6b3, 0x0100e9d4, 0xe9d44408, 0x44111202, 0x69214408, 0x69614408, 0x69a14408, 
-    0x42404408, 0x0b2861e0, 0xbf242810, 0xeb01210e, 0x213200d0, 0xf8df4e1e, 0x444e807c, 0x1000e9c6, 
-    0xf10660b0, 0x46300114, 0x697047c0, 0x2033b988, 0x0500e9c6, 0x7000f44f, 0x4002e9c6, 0x613068b8, 
-    0x0114f106, 0x47c04630, 0x28006970, 0xe8bdbf08, 0x200181f0, 0x81f0e8bd, 0x1040eb01, 0xeb01490e, 
-    0x68010080, 0x0107f021, 0x68016001, 0x60014311, 0x00004770, 0x00000004, 0x20098000, 0x000000b4, 
-    0x400fc000, 0x1fff1ff8, 0xcccccccd, 0x00000034, 0x00000014, 0x1fff1ff1, 0x4002c000, 0x00000000, 
-    0x00000001, 0x00000000, 0x00000000, 0x00000000, 
+    0x28100b00, 0x08c0d301, 0x4770300e, 0x4c60b570, 0x444c2500, 0x61652036, 0x60204621, 0x31144a5d,
+    0x47904620, 0x28006960, 0x2001d001, 0x69a0bd70, 0x4959227d, 0x01520e00, 0x28274449, 0xdc02d01c,
+    0xd1062820, 0x2828e018, 0x2847d016, 0x2848d014, 0x600ad012, 0x60cd4951, 0x3a804a50, 0x23aa6015,
+    0x235560d3, 0x220160d3, 0xd0112828, 0x2820dc06, 0x2827d00e, 0xe00bd106, 0xe7ea4a49, 0xd0072847,
+    0xd0052848, 0x4845604d, 0x600238c0, 0xbd702000, 0xe7f8604a, 0x47702000, 0x4c3db5f8, 0x444c2032,
+    0x261d2500, 0xc2614622, 0x31144621, 0x46204f39, 0x47b89100, 0x28006960, 0x2034d10c, 0xc1614621,
+    0x44484835, 0x60e06800, 0x99004620, 0x696047b8, 0xd0002800, 0xbdf82001, 0xf7ffb5f8, 0x4d2cff91,
+    0x444d4604, 0x606c2032, 0x60ac4629, 0x60283114, 0x460f4e28, 0x47b04628, 0x28006968, 0x606cd10d,
+    0x60ac2034, 0x48246028, 0x44484639, 0x60e86800, 0x47b04628, 0x28006968, 0x2001d000, 0xb5f8bdf8,
+    0x46140006, 0x6861d10e, 0x68e26820, 0x68a11840, 0x18401889, 0x18406921, 0x18406961, 0x184069a1,
+    0x61e04240, 0xf7ff4630, 0x4d11ff5b, 0x444d2132, 0x60296068, 0x462960a8, 0x4f0e3114, 0x91004628,
+    0x696847b8, 0xd1102800, 0x2033606e, 0x602860ac, 0x02802001, 0x480860e8, 0x68004448, 0x46286128,
+    0x47b89900, 0x28006968, 0x2001d000, 0x0000bdf8, 0x00000008, 0x1fff1ff1, 0x00000004, 0x400fc100,
+    0x00002ee0, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000, 0x00000000,
+    0x00000000, 0x00000000, 0x00000000
+};
+
+// Start address of flash
+static const uint32_t flash_start = 0x00000000;
+// Size of flash
+static const uint32_t flash_size = 0x00080000;
+
+/**
+* List of start and size for each size of flash sector - even indexes are start, odd are size
+* The size will apply to all sectors between the listed address and the next address
+* in the list.
+* The last pair in the list will have sectors starting at that address and ending
+* at address flash_start + flash_size.
+*/
+static const uint32_t sectors_info[] = {
+    0x00000000, 0x00001000,
+    0x00010000, 0x00008000,
 };
 
 static const program_target_t flash = {
-    0x200000D5, // Init
-    0x200001D9, // UnInit
-    0x200001DD, // EraseChip
-    0x20000227, // EraseSector
-    0x20000281, // ProgramPage
+    0x2000002d, // Init
+    0x200000b5, // UnInit
+    0x200000b9, // EraseChip
+    0x200000f9, // EraseSector
+    0x2000013f, // ProgramPage
 
     // BKPT : start of blob + 1
     // RSB  : blob start + header + rw data offset
     // RSP  : stack pointer
     {
-        0x20000000 + 0x00000001,
-        0x20000000 + 0x00000020 + 0x00000400,
+        0x20000001,
+        0x200001c4,
         0x20000800
     },
 
-    0x20000000 + 0x00000A00,   // mem buffer location
-    0x20000000,                // location to write prog_blob in target RAM
-    sizeof(lpc4088_flash_prog_blob), // prog_blob size
-    lpc4088_flash_prog_blob,         // address of prog_blob
-    0x00000200                 // ram_to_flash_bytes_to_be_written
+    0x20000000 + 0x00000A00,  // mem buffer location
+    0x20000000,               // location to write prog_blob in target RAM
+    sizeof(_flash_prog_blob),   // prog_blob size
+    _flash_prog_blob,           // address of prog_blob
+    0x00000400       // ram_to_flash_bytes_to_be_written
 };
--- source/target/siliconlabs/efm32gg/flash_blob.c
+++ source/target/siliconlabs/efm32gg/flash_blob.c
@@ -1,70 +1,74 @@
-/**
- * @file    flash_blob.c
- * @brief   Flash algorithm for the efm32gg
+/* Flash OS Routines (Automagically Generated)
+ * Copyright (c) 2009-2015 ARM Limited
  *
- * DAPLink Interface Firmware
- * Copyright (c) 2009-2016, ARM Limited, All Rights Reserved
- * SPDX-License-Identifier: Apache-2.0
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
 
-#include "flash_blob.h"
-
-static const uint32_t efm32gg_flash_prog_blob[] = {
+static const uint32_t _flash_prog_blob[] = {
     0xE00ABE00, 0x062D780D, 0x24084068, 0xD3000040, 0x1E644058, 0x1C49D1FA, 0x2A001E52, 0x4770D1F2,
-    0x4770ba40, 0x4770bac0, 0x4c0cb510, 0xf04068a0, 0x60a00001, 0x301af246, 0xf44f6560, 0x60e07040,
-    0x20012100, 0xf83ef000, 0x65612100, 0xf02168a1, 0x60a10101, 0xbf182800, 0xbd102001, 0x400c0000,
-    0x4684b510, 0xf44f2300, 0xf8505180, 0x1f092b04, 0x1c52d002, 0xe001d0f9, 0xd0131c50, 0x68a04c0c,
-    0x0001f040, 0xf8c460a0, 0x2001c010, 0x200260e0, 0x210060e0, 0xf0002001, 0x4603f815, 0xf02068a0,
-    0x60a00001, 0xbf142b00, 0x20002001, 0x0000bd10, 0x400c0000, 0xf6414902, 0x63c83071, 0x47702000,
-    0x400c0000, 0x4b14b410, 0xc050f8df, 0x201cf8dc, 0x0f16f012, 0xf8dcd014, 0xf0200008, 0xf8cc0005,
-    0xf0120008, 0xbf1e0f02, 0x0001f06f, 0x4770bc10, 0x0f04f012, 0xf04fbf1e, 0xbc1030ff, 0xe0074770,
-    0x428a4002, 0xbc10d102, 0x47702000, 0xd1dd1e5b, 0xf06fbc10, 0x47700002, 0x00989680, 0x400c0000,
-    0x4df0e92d, 0x1cc84604, 0x0603f020, 0x46904859, 0xf0416881, 0x60810101, 0xd9732e07, 0x0f07f014,
-    0xf8d8d011, 0x61041000, 0x60c22201, 0x21086181, 0x210060c1, 0xf7ff4610, 0x2800ffb5, 0x1d24d17e,
-    0x0810f108, 0x484b1f36, 0xf0416881, 0x60810104, 0xbf882e07, 0x0a02f06f, 0xf504d966, 0x46015080,
-    0x010bf36f, 0x42b11b09, 0x4635bf8e, 0x000bf36f, 0xf0151b05, 0xbf180f04, 0x46471f2d, 0x20012100,
-    0xff90f7ff, 0xd13b2800, 0xe0e8f8df, 0x4010f8ce, 0xf8ce2001, 0xf857000c, 0xf8ce0b04, 0xf8570018,
-    0xf8ce0b04, 0x20080018, 0x000cf8ce, 0x0208f1a5, 0xd9332a07, 0x0b08f04f, 0x46dc482f, 0x101cf8de,
-    0x0f16f011, 0xf8ded012, 0xf0200008, 0xf8ce0005, 0xf0110008, 0xbf180f02, 0x0001f06f, 0xf011d10f,
-    0xbf180f04, 0x30fff04f, 0xe007d109, 0x010bea01, 0xd1014561, 0xe0022000, 0xd1df1e40, 0xb1104650,
-    0xe00bb9e0, 0xf857e01c, 0xf8ce0b04, 0xf8570018, 0xf8ce0b04, 0x3a080018, 0xd8cb2a07, 0xeb081b76,
-    0x442c0885, 0xd8982e07, 0x20012100, 0xff3af7ff, 0x4810b920, 0xf0216881, 0xe0000104, 0x6081e00f,
-    0x480cb186, 0x1000f8d8, 0x22016104, 0x618160c2, 0x60c12108, 0x46102100, 0xff24f7ff, 0x2001b110,
-    0x8df0e8bd, 0x68814803, 0x0101f021, 0x20006081, 0x8df0e8bd, 0x400c0000, 0x00989680, 0x68814803,
-    0x0105f021, 0x20006081, 0x00004770, 0x400c0000, 0x00000000,
+    0x4770ba40, 0x4770bac0, 0x0030ea4f, 0x00004770, 0x4b75b570, 0xf06f4c75, 0x69e20502, 0x0f16f012,
+    0x68a0d00d, 0x0005f020, 0x079060a0, 0xf06fd502, 0xbd700001, 0xd5090750, 0x30fff04f, 0x4002bd70,
+    0xd101428a, 0xbd702000, 0xd1e61e5b, 0xbd704628, 0xf6414966, 0x63c83071, 0x47702000, 0x68814863,
+    0x0105f021, 0x20006081, 0xb5104770, 0x68a04c5f, 0x0001f040, 0xf24660a0, 0x6560301a, 0x7040f44f,
+    0xf880f000, 0x65612100, 0xf02168a1, 0x60a10101, 0xd0002800, 0xbd102001, 0x4601b510, 0x5380f44f,
+    0x1f1bc904, 0x0201f102, 0x2a00d002, 0xe000d0f8, 0x4c4eb18a, 0xf04168a1, 0x60a10101, 0x20016120,
+    0x200260e0, 0xf85ef000, 0xf02168a1, 0x60a10101, 0x2001b108, 0x2000bd10, 0xe92dbd10, 0xf8df41f0,
+    0x1cc9810c, 0x0503f021, 0x1008f8d8, 0x46064617, 0x0101f041, 0x1008f8c8, 0xd9322d07, 0xd0070770,
+    0x68394630, 0xf843f000, 0x1f2dbb88, 0x1d3f1d36, 0x1008f8d8, 0x0104f041, 0x1008f8c8, 0xbf00e014,
+    0x5080f506, 0x000bf36f, 0x42ac1b84, 0x462cd900, 0xd5000760, 0x46221f24, 0x46304639, 0xf82ef000,
+    0x4427b9a8, 0x1b2d4426, 0xd8e92d07, 0x20012100, 0xff5ef7ff, 0xf8d8b958, 0xf0211008, 0xf8c80104,
+    0xb13d1008, 0x68394630, 0xf811f000, 0x2001b110, 0x81f0e8bd, 0x1008f8d8, 0x0101f021, 0x1008f8c8,
+    0xe7f52000, 0x60c84919, 0x20012100, 0x4a17e740, 0x20016110, 0x619160d0, 0xe7f32008, 0x41f0e92d,
+    0x4607460c, 0x21004616, 0xf7ff2001, 0x2800ff31, 0x4d0ed1de, 0x2001612f, 0xcc0160e8, 0xcc0161a8,
+    0x200861a8, 0xe00960e8, 0x46082108, 0xff20f7ff, 0xd1cd2800, 0x61a8cc01, 0x61a8cc01, 0x2e073e08,
+    0x2000d8f2, 0x0000e7c4, 0x00989680, 0x400c0000, 0x00000000
+};
+
+// Start address of flash
+static const uint32_t flash_start = 0x00000000;
+// Size of flash
+static const uint32_t flash_size = 0x00100000;
+
+/**
+* List of start and size for each size of flash sector - even indexes are start, odd are size
+* The size will apply to all sectors between the listed address and the next address
+* in the list.
+* The last pair in the list will have sectors starting at that address and ending
+* at address flash_start + flash_size.
+*/
+static const uint32_t sectors_info[] = {
+    0x00000000, 0x00001000,
 };
 
 static const program_target_t flash = {
-    0x200000B5, // Init
-    0x2000029D, // UnInit
-    0x20000029, // EraseChip
-    0x20000061, // EraseSector
-    0x20000121, // ProgramPage
+    0x20000071, // Init
+    0x2000007d, // UnInit
+    0x2000008b, // EraseChip
+    0x200000b9, // EraseSector
+    0x200000fb, // ProgramPage
 
     // BKPT : start of blob + 1
     // RSB  : blob start + header + rw data offset
     // RSP  : stack pointer
     {
-        0x20000000 + 0x00000001,
-        0x20000000 + 0x00000020 + 0x00000290,
+        0x20000001,
+        0x20000210,
         0x20000800
     },
 
-    0x20000000 + 0x00000A00,   // mem buffer location
-    0x20000000,                // location to write prog_blob in target RAM
-    sizeof(efm32gg_flash_prog_blob), // prog_blob size
-    efm32gg_flash_prog_blob,         // address of prog_blob
-    0x00000200                 // ram_to_flash_bytes_to_be_written
+    0x20000000 + 0x00000A00,  // mem buffer location
+    0x20000000,               // location to write prog_blob in target RAM
+    sizeof(_flash_prog_blob),   // prog_blob size
+    _flash_prog_blob,           // address of prog_blob
+    0x00004000       // ram_to_flash_bytes_to_be_written
 };
